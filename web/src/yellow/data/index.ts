e0fface96eae17e19b4d6942552c7a2ffe50fc6e
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-03-18 21:12:44 +0100

    Data module interface

---
+++ web/src/yellow/data/index.ts
@@ -0,0 +1,20 @@
+export enum ConflictMode {
+    skip,
+    override,
+    error
+}
+
+export interface DataWrapper<DataType,KeyType,OptCounterType> {
+    data: DataType
+    key: KeyType
+    optCounter?: OptCounterType
+}
+
+export interface DataAccess<DataType,KeyType,OptCounterType, QueryType> {
+    get(key: KeyType, optCounter?: OptCounterType): Promise<DataWrapper<DataType,KeyType,OptCounterType>>
+    set(key: KeyType, value: DataType, optCounter?: OptCounterType)
+    update(key: KeyType, value: DataType, conflict?: ConflictMode)
+    merge(key: KeyType, value: DataType, merge: (a: DataType, b: DataType) => DataType)
+    delete(key: KeyType, optCounter?: OptCounterType)
+    list(query?: QueryType): Promise<DataWrapper<DataType,KeyType,OptCounterType>[]>
+}
