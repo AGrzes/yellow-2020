a111f403954f6dfe90913df9c56ccfd084baa939
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-03-21 13:45:26 +0100

    Simple UI Model

---
+++ web/src/yellow/ui-model/index.ts
@@ -0,0 +1,12 @@
+
+export interface EntityView {
+    pathName: string
+    dataModel: string
+    listItemTemplate: string
+    detailsTemplate: string
+}
+
+
+export interface UIModel {
+    views: EntityView[]
+}
