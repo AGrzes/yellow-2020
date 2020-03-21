4f4770f30b92eae520c359e7ba61b952b884d1f5
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-03-21 16:50:43 +0100

    Ui state test

---
+++ web/test/yellow/state/ui.ts
@@ -0,0 +1,17 @@
+import 'mocha'
+import uiState from '../../../src/yellow/state/ui'
+import {expect}  from 'chai'
+import { UIModel } from '../../../src/yellow/ui-model'
+
+describe('uiState', function() {
+
+    it('Should expose provided ui model',async function() {
+        const model = {
+
+        } as UIModel
+        const uiModule = uiState(model)
+        expect(uiModule).to.have.property('state',model)
+        expect(uiModule).to.have.property('namespaced',true)
+        expect(uiModule).to.have.nested.property('actions.fetch')
+    })
+})
