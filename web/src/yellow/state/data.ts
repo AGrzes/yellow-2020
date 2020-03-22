505e3ebcbcc482a871954f64b7397a89841f2459
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-03-22 15:18:49 +0100

    Improved data state

--- web/src/yellow/state/data.ts
+++ web/src/yellow/state/data.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash'
 import { Module } from 'vuex'
 import { DataAccess } from '../data'
+import Vue from 'vue'
 
 export default function dataState<S,R>(access: DataAccess<S,string,string,any>): Module<{[key:string]:S},R> {
     return {
@@ -9,13 +10,16 @@ export default function dataState<S,R>(access: DataAccess<S,string,string,any>):
         },
         mutations: {
             update(state, data: {[key:string]:S}) {
-                _.assign(state,data)
+                _.forEach(data,(value,key) => Vue.set(state,key,value))
             }
         },
         actions: {
             async fetch(context, key?: string) {
                 if (key) {
-                    context.commit('update', { [key]:(await access.get(key)).data})
+                    const item = await access.get(key)
+                    if (item) {
+                        context.commit('update', { [key]: item.data})
+                    }
                 } else {
                     context.commit('update', _.mapValues(_.keyBy(await access.list(),'key'),'data'))
                 }
