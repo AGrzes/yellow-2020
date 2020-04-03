f4d736c6bd31ef60232ceb39dd73606ed77176bb
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-04-03 21:21:53 +0200

    Using model state

--- web/src/app.ts
+++ web/src/app.ts
@@ -3,11 +3,14 @@ import './components'
 import router from './router'
 import store from './state'
 /* eslint-disable no-unused-vars */
-const app = new Vue({
-  el: 'body #app',
-  template: `<router-view></router-view>`,
-  store,
-  router,
-  data: {
-  }
-})
+(async () => {
+  const app = new Vue({
+    el: 'body #app',
+    template: `<router-view></router-view>`,
+    store: await store(),
+    router,
+    data: {
+    }
+  })
+})()
+
