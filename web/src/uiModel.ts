65394d5ee702421555547ce6f80f486567971f02
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-04-05 19:39:21 +0200

    Author pages

--- web/src/uiModel.ts
+++ web/src/uiModel.ts
@@ -9,5 +9,14 @@ export const uiModel: UIModel = {
       <h1>{{item.title}}</h1>
       <h2><small v-for="author in item.author"> {{author.name}}</small></h2>
       `
+    },{
+      dataModel: 'author',
+      pathName: 'authors',
+      listItemTemplate: `<h1>{{item.name}}</h1>`,
+      detailsTemplate: `
+      <h1>{{item.name}}</h1>
+      <h2>Books</h2>
+      <ul><li v-for="book in item.books"> {{book.title}}</li></ul>
+      `
     }]
   }
