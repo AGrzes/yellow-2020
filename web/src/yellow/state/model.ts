222cc14d60acea30173ecf68c15a9d7d32aa5314
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-04-12 17:57:12 +0200

    Item change handling

--- web/src/yellow/state/model.ts
+++ web/src/yellow/state/model.ts
@@ -2,20 +2,34 @@ import * as _ from 'lodash'
 import { Module } from 'vuex'
 import { Model } from '../model'
 import { Class } from '../metadata'
-
+import Vue from 'vue'
 
 export class ModelStateAdapter {
     public constructor(private model: Model) {}
 
     public state<R>(type: Class): Module<any,R> {
+        const model=this.model
         return {
             namespaced: true,
             state: {
                 ...this.model.map(type)
             },
+            mutations: {
+                update(state,{key,value}) {
+                    Vue.set(state,key,value)
+                }
+            },
             actions: {
                 async fetch(context) {
                     //
+                },
+                async raw(context,{key,value}) {
+                    if (value) {
+                        await model.raw(type,key,value)
+                        context.commit('update',{key,value: model.get(type,key)})
+                    } else {
+                        return model.raw(type,key)
+                    }
                 }
             }
         }
