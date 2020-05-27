daa4291183fccaf732197c59b479e79c7e7714bd
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-05-27 20:15:33 +0200

    Updated ui model definition

--- modules/common/ui-model/src/index.ts
+++ modules/common/ui-model/src/index.ts
@@ -1,11 +1,46 @@
 
+export interface ItemsUI {
+  kind: string
+}
+
+export interface ListUI extends ItemsUI {
+  kind: 'list'
+  itemTemplate: string
+}
+
+export function isListUI(itemsUI: ItemsUI): itemsUI is ListUI {
+  return itemsUI.kind === 'list'
+}
+
+export interface CardsUI extends ItemsUI {
+  kind: 'cards'
+  cardTemplate: string
+}
+
+export function isCardsUI(itemsUI: ItemsUI): itemsUI is CardsUI {
+  return itemsUI.kind === 'cards'
+}
+
+export interface ColumnDefinition {
+  headerTemplate: string
+  itemTemplate: string
+}
+
+export interface TableUI extends ItemsUI {
+  kind: 'table'
+  columns: ColumnDefinition[]
+}
+
+export function isTableUI(itemsUI: ItemsUI): itemsUI is TableUI {
+  return itemsUI.kind === 'table'
+}
+
 export interface EntityView {
     pathName: string
     dataModel: string
-    listItemTemplate: string
+    itemsUI: ItemsUI
     detailsTemplate: string
 }
-
 
 export interface UIModel {
     views: EntityView[]
--- modules/common/ui-model/types/index.d.ts
+++ modules/common/ui-model/types/index.d.ts
@@ -1,7 +1,29 @@
+export interface ItemsUI {
+    kind: string;
+}
+export interface ListUI extends ItemsUI {
+    kind: 'list';
+    itemTemplate: string;
+}
+export declare function isListUI(itemsUI: ItemsUI): itemsUI is ListUI;
+export interface CardsUI extends ItemsUI {
+    kind: 'cards';
+    cardTemplate: string;
+}
+export declare function isCardsUI(itemsUI: ItemsUI): itemsUI is CardsUI;
+export interface ColumnDefinition {
+    headerTemplate: string;
+    itemTemplate: string;
+}
+export interface TableUI extends ItemsUI {
+    kind: 'table';
+    columns: ColumnDefinition[];
+}
+export declare function isTableUI(itemsUI: ItemsUI): itemsUI is TableUI;
 export interface EntityView {
     pathName: string;
     dataModel: string;
-    listItemTemplate: string;
+    itemsUI: ItemsUI;
     detailsTemplate: string;
 }
 export interface UIModel {
