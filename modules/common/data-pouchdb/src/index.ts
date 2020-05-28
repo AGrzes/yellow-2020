66c90cc2367695c95c0d1f077857151b252a3219
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-05-28 21:36:39 +0200

    Fixed rev issue with couchdb

--- modules/common/data-pouchdb/src/index.ts
+++ modules/common/data-pouchdb/src/index.ts
@@ -1,40 +1,39 @@
-import pouchdbCore from 'pouchdb-core'
-import pouchdbAdapterHttp from 'pouchdb-adapter-http'
-import pouchdbMapReduce from 'pouchdb-mapreduce'
-import { DataAccess, ConflictMode, DataWrapper } from '@agrzes/yellow-2020-common-data'
+import { ConflictMode, DataAccess, DataWrapper } from '@agrzes/yellow-2020-common-data'
 import * as _ from 'lodash'
+import pouchdbAdapterHttp from 'pouchdb-adapter-http'
+import pouchdbCore from 'pouchdb-core'
+import pouchdbMapReduce from 'pouchdb-mapreduce'
 
 export const PouchDB = pouchdbCore.plugin(pouchdbAdapterHttp).plugin(pouchdbMapReduce)
 
-
-export class PouchDBDataAccess<DataType extends object> implements DataAccess<DataType, string,string, void> {
-    async get(key: string, optCounter?: string): Promise<DataWrapper<DataType,string,string>> {
+export class PouchDBDataAccess<DataType extends object> implements DataAccess<DataType, string, string, void> {
+    public async get(key: string, optCounter?: string): Promise<DataWrapper<DataType, string, string>> {
         try {
-            const result = await this.db.get(key,{rev: optCounter})
+            const result = await this.db.get(key, {rev: optCounter})
             return {
-                key:result._id,
+                key: result._id,
                 data: result,
                 optCounter: result._rev
             }
         } catch (e) {
-            if (e.name === 'not_found'){
+            if (e.name === 'not_found') {
                 return null
             } else {
                 throw e
             }
         }
     }
-    async set(key: string, value: DataType, optCounter?: string): Promise<string> {
+    public async set(key: string, value: DataType, optCounter?: string): Promise<string> {
         return (await this.db.put({_id: key, ...value, _rev: optCounter})).rev
     }
-    async update(key: string, value: DataType, conflict?: ConflictMode) {
-        let optCounter = null
+    public async update(key: string, value: DataType, conflict?: ConflictMode) {
+        let optCounter: string
         while (true) {
             try {
                 await this.db.put({_id: key, ...value, _rev: optCounter})
                 break
             } catch (e) {
-                if (e.name === 'conflict'){
+                if (e.name === 'conflict') {
                     if (conflict === ConflictMode.skip) {
                         break
                     } else if (conflict === ConflictMode.override) {
@@ -48,30 +47,30 @@ export class PouchDBDataAccess<DataType extends object> implements DataAccess<Da
             }
         }
     }
-    async merge(key: string, value: DataType, merge: (a: DataType, b: DataType) => DataType) {
-        let optCounter = null
+    public async merge(key: string, value: DataType, merge: (a: DataType, b: DataType) => DataType) {
+        let optCounter: string
         while (true) {
             try {
                 await this.db.put({_id: key, ...value, _rev: optCounter})
                 break
             } catch (e) {
-                if (e.name === 'conflict'){
+                if (e.name === 'conflict') {
                     const existing = await this.db.get(key)
                     optCounter = existing._rev
-                    value = merge(value,existing)
+                    value = merge(value, existing)
                 } else {
                     throw e
                 }
             }
         }
     }
-    async delete(key: string, optCounter?: string) {
+    public async delete(key: string, optCounter?: string) {
         if (!optCounter) {
             optCounter = (await this.db.get(key))._rev
         }
-        await this.db.remove(key,optCounter)
+        await this.db.remove(key, optCounter)
     }
-    async list(query?: void): Promise<DataWrapper<DataType,string,string>[]> {
+    public async list(query?: void): Promise<Array<DataWrapper<DataType, string, string>>> {
         return (await this.db.allDocs({include_docs: true})).rows.map(({id, value: { rev}, doc}) => ({
             data: doc,
             key: id,
--- modules/common/data-pouchdb/types/index.d.ts
+++ modules/common/data-pouchdb/types/index.d.ts
@@ -3,7 +3,7 @@
 /// <reference types="pouchdb-adapter-memory" />
 /// <reference types="pouchdb-find" />
 /// <reference types="pouchdb-mapreduce" />
-import { DataAccess, ConflictMode, DataWrapper } from '@agrzes/yellow-2020-common-data';
+import { ConflictMode, DataAccess, DataWrapper } from '@agrzes/yellow-2020-common-data';
 export declare const PouchDB: PouchDB.Static;
 export declare class PouchDBDataAccess<DataType extends object> implements DataAccess<DataType, string, string, void> {
     private db;
@@ -12,6 +12,6 @@ export declare class PouchDBDataAccess<DataType extends object> implements DataA
     update(key: string, value: DataType, conflict?: ConflictMode): Promise<void>;
     merge(key: string, value: DataType, merge: (a: DataType, b: DataType) => DataType): Promise<void>;
     delete(key: string, optCounter?: string): Promise<void>;
-    list(query?: void): Promise<DataWrapper<DataType, string, string>[]>;
+    list(query?: void): Promise<Array<DataWrapper<DataType, string, string>>>;
     constructor(db: PouchDB.Database<DataType>);
 }
