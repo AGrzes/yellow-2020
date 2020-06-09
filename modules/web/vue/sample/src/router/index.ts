968222e806a56345cf3ff6a15cb447dfc1771728
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-06-09 22:47:37 +0200

    Navigation usage

--- modules/web/vue/sample/src/router/index.ts
+++ modules/web/vue/sample/src/router/index.ts
@@ -2,10 +2,35 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import { uiModel } from '../uiModel'
 import { modelRoutes } from '@agrzes/yellow-2020-web-vue-router'
+import { navigationMenu } from '@agrzes/yellow-2020-web-vue-components'
 Vue.use(VueRouter)
 
 async function router() {
-    const routes: RouteConfig[] = [...modelRoutes(await uiModel())]
+    const model = await uiModel()
+    const routes: RouteConfig[] = [{
+        name: 'references',
+        path: '/references',
+        component: {
+          template: `
+            <div class="row mt-4">
+              <div class="col-2">
+                <navigation-menu :navigation="navigation"></navigation-menu>
+              </div>
+              <div class="col-10">
+                <router-view></router-view>
+              </div>
+           </div>`,
+           data() {
+               return {
+                   navigation: model.navigation
+               }
+           },
+           components: {
+               navigationMenu
+           }
+        },
+        children: modelRoutes(await uiModel())
+      }]
     return new VueRouter({
         mode: 'history',
         routes,
