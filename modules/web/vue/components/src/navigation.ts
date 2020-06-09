aacb24e0e6f209f72fddfe870c9777a27b521c6c
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-06-09 22:45:13 +0200

    Navigation component

---
+++ modules/web/vue/components/src/navigation.ts
@@ -0,0 +1,31 @@
+
+import Vue from 'vue'
+import {MenuItem} from '@agrzes/yellow-2020-common-ui-model'
+
+export const navigationMenu = Vue.extend({
+  props: {
+    navigation: Array as () => MenuItem[]
+  },
+  template: `
+<ul class="nav flex-column">
+    <menu-item :item="item"  v-for="(item,key) in navigation" :key="key"></menu-item>
+</ul>
+  `,
+  components: {
+    menuItem: {
+      name: 'menu-item',
+      props: {
+        item: Object as () => MenuItem
+      },
+      template: `
+      <router-link tag="li" class="nav-item" :to="item.route">
+        <a class="nav-link">{{item.label}}</a>
+        <ul class="list-unstyled pl-3" v-if="item.children">
+            <menu-item :item="childItem" v-for="(childItem,key) in item.children" :key="key"></menu-item>
+        </ul>
+      </router-link>
+      
+      `
+    }
+  }
+})
