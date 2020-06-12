0bdc3336d05f44d6d0b38ee970097d9a7c6710af
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-06-12 22:21:48 +0200

    Swithced to config based metadata initialization

--- modules/local/sample/src/loadSchema.ts
+++ modules/local/sample/src/loadSchema.ts
@@ -1,11 +1,12 @@
 import { ConflictMode} from '@agrzes/yellow-2020-common-data'
 import { PouchDB , PouchDBDataAccess} from '@agrzes/yellow-2020-common-data-pouchdb'
-import {ModelAccess} from '@agrzes/yellow-2020-common-metadata'
+import {ModelAccess, ModelDescriptor} from '@agrzes/yellow-2020-common-metadata'
 import debug from 'debug'
 import fs from 'fs'
 import YAML from 'js-yaml'
 import _ from 'lodash'
 import {basename, extname, join} from 'path'
+import { loadModelDb } from './modelLoader'
 
 const log = debug('agrzes:yellow-2020-local-sample')
 
@@ -14,15 +15,15 @@ const readdir = fs.promises.readdir
 
 const yamlExt = '.yaml'
 
-async function loadModels(dir: string): Promise<ModelAccess> {
+async function loadModels(dir: string): Promise<Record<string, ModelDescriptor>> {
   const files = (await readdir(dir)).filter((file) => extname(file) === yamlExt)
-  const models = _.fromPairs(await Promise.all(_.map(files, async (file) =>
+  const models: Record<string, ModelDescriptor> = _.fromPairs(await Promise.all(_.map(files, async (file) =>
     [basename(file, yamlExt), YAML.safeLoad(await readFile(join(dir, file), 'utf-8'))])))
-  return {models}
+  return models
 }
 
-async function saveModels({models}: ModelAccess) {
-  const modelDb = new PouchDBDataAccess(new PouchDB('http://localhost:5984/model'))
+async function saveModels(models: Record<string, ModelDescriptor>) {
+  const modelDb = await loadModelDb()
   await Promise.all(_.map(models, (model, name) => modelDb.update(name, model, ConflictMode.override)))
 }
 
