0cbf4e9ae847f181a1b840d8a7f1a874e5b859ff
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-06-27 22:02:03 +0200

    Fixed formatting

--- modules/web/vue/state/src/index.ts
+++ modules/web/vue/state/src/index.ts
@@ -1,10 +1,11 @@
-import Vuex, { Store } from 'vuex'
-import { PouchDBDataAccess , PouchDB} from '@agrzes/yellow-2020-common-data-pouchdb'
-import { setupModel, SimpleTypedDataAccess, TypedDataAccess, Model } from '@agrzes/yellow-2020-common-model'
-import { SimpleModelAccess, Class, ModelAccess } from '@agrzes/yellow-2020-common-metadata'
-import { ModelStateAdapter } from './model'
-import * as _ from 'lodash'
+import { PouchDB , PouchDBDataAccess} from '@agrzes/yellow-2020-common-data-pouchdb'
 import { DataAccess } from '@agrzes/yellow-2020-common-data/types'
+import { Class, ModelAccess, SimpleModelAccess } from '@agrzes/yellow-2020-common-metadata'
+import { Model, setupModel as setupModelImpl,
+  SimpleTypedDataAccess, TypedDataAccess } from '@agrzes/yellow-2020-common-model'
+import * as _ from 'lodash'
+import Vuex, { Store } from 'vuex'
+import { ModelStateAdapter } from './model'
 
 export interface StateConfig {
     metadata: string
@@ -17,10 +18,12 @@ export interface StateConfig {
 }
 
 type SetupMetadata = (config: string) => Promise<ModelAccess>
-type SetupModleAccess<T> = (dataAccess: DataAccess<T,string, string, void>) => Promise<ModelAccess>
-type SetupDataAccess<T> = (url: string) => DataAccess<T,string, string, void>
-type SetupTypedDataAccess<T> = (type: Class, wrapped: DataAccess<T,string, string, void>) => TypedDataAccess<T, string, string, void>;
-type SetupModel = (metaModel: ModelAccess, dataAccess: TypedDataAccess<object, string, string, never>[]) => Promise<Model>
+type SetupModleAccess<T> = (dataAccess: DataAccess<T, string, string, void>) => Promise<ModelAccess>
+type SetupDataAccess<T> = (url: string) => DataAccess<T, string, string, void>
+type SetupTypedDataAccess<T> = (type: Class, wrapped: DataAccess<T, string, string, void>) =>
+  TypedDataAccess<T, string, string, void>
+type SetupModel = (metaModel: ModelAccess, dataAccess: Array<TypedDataAccess<object, string, string, never>>) =>
+  Promise<Model>
 type SetupModelStateAdapter = (model: Model) => ModelStateAdapter
 
 export function setupSetupModelStateAdapter(): SetupModelStateAdapter {
@@ -40,17 +43,19 @@ export function setupSetupMetadata(
 }
 
 export function setupStore(setupMetadata: SetupMetadata,
-    setupDataAccess: SetupDataAccess<any>,
-    setupTypedDataAccess: SetupTypedDataAccess<any>,
-    setupModel: SetupModel,
-    setupModelStateAdapter: SetupModelStateAdapter): (config:StateConfig) => Promise<Store<unknown>> {
+                           setupDataAccess: SetupDataAccess<any>,
+                           setupTypedDataAccess: SetupTypedDataAccess<any>,
+                           setupModel: SetupModel,
+                           setupModelStateAdapter: SetupModelStateAdapter):
+                           (config: StateConfig) => Promise<Store<unknown>> {
   return async (config) => {
     const metadata = await setupMetadata(config.metadata)
-    const data = _.map(config.data,(path, url) => setupTypedDataAccess(_.get(metadata.models,path) as unknown as Class,setupDataAccess(url)))
-    const model = await setupModel( metadata,data)
+    const data = _.map(config.data, (path, url) =>
+      setupTypedDataAccess(_.get(metadata.models, path) as unknown as Class, setupDataAccess(url)))
+    const model = await setupModel( metadata, data)
     const adapter = setupModelStateAdapter(model)
     return new Vuex.Store({
-      modules: _.mapValues(config.stores,(path) => adapter.state(_.get(metadata.models,path) as unknown as Class))
+      modules: _.mapValues(config.stores, (path) => adapter.state(_.get(metadata.models, path) as unknown as Class))
     })
   }
 }
@@ -58,6 +63,6 @@ export function setupStore(setupMetadata: SetupMetadata,
 export const store = setupStore(
   setupSetupMetadata(setupSetupDataAccess(PouchDB, PouchDBDataAccess), SimpleModelAccess.loadFromAdapter),
   setupSetupDataAccess(PouchDB, PouchDBDataAccess),
-  <T>(type: Class, wrapped: DataAccess<T,string, string, void>) => new SimpleTypedDataAccess(type,wrapped),
-  setupModel,
+  <T>(type: Class, wrapped: DataAccess<T, string, string, void>) => new SimpleTypedDataAccess(type, wrapped),
+  setupModelImpl,
   setupSetupModelStateAdapter())
