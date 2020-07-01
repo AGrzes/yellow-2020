a1c700f7c626626fee69130e9089267fdae9574b
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-07-01 22:21:54 +0200

    Updated model

--- modules/web/vue/books/src/model.ts
+++ modules/web/vue/books/src/model.ts
@@ -1,33 +1,51 @@
 
-export interface Book<Ref = never> {
-  title: string
-  description?: string
-  author: Array<Ref | Author<Ref>>
-  genre: Array<Ref | Genre<Ref>>
-  libraries: Array<LibraryEntry<Ref>>
+import _ from 'lodash'
+
+export class Book<Ref = never> {
+  public title: string
+  public description?: string
+  public author: Array<Ref | Author<Ref>>
+  public genre: Array<Ref | Genre<Ref>>
+  public libraries: Array<LibraryEntry<Ref>>
+  public static typeTag = 'book'
+  public static key(book: Book) {
+    return _.snakeCase(book.title)
+  }
 }
 
-export interface Author<Ref = never> {
-  name: string
-  books: Array<Ref | Book<Ref>>
+export class Author<Ref = never> {
+  public name: string
+  public books: Array<Ref | Book<Ref>>
+  public static typeTag = 'author'
+  public static key(author: Author) {
+    return _.snakeCase(author.name)
+  }
 }
 
-export interface Genre<Ref = never> {
-  name: string
-  books: Array<Ref | Book<Ref>>
+export class Genre<Ref = never> {
+  public name: string
+  public books: Array<Ref | Book<Ref>>
+  public static typeTag = 'genre'
+  public static key(genre: Genre) {
+    return _.snakeCase(genre.name)
+  }
 }
 
-export interface Library<Ref = never> {
-  name: string
-  description?: string
-  kind: 'physical' | 'digital'
-  entries: Array<Ref | LibraryEntry<Ref>>
+export class Library<Ref = never> {
+  public name: string
+  public description?: string
+  public kind: 'physical' | 'digital'
+  public entries: Array<Ref | LibraryEntry<Ref>>
+  public static typeTag = 'library'
+  public static key(library: Library) {
+    return _.snakeCase(library.name)
+  }
 }
 
-export interface LibraryEntry<Ref = never> {
-  owned: boolean
-  price?: number
-  url?: string
-  library: Ref | Library<Ref>
-  book?: Ref | Book<Ref>
+export class LibraryEntry<Ref = never> {
+  public owned: boolean
+  public price?: number
+  public url?: string
+  public library: Ref | Library<Ref>
+  public book?: Ref | Book<Ref>
 }
