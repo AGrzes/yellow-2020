408de349fa43b3c16d9799c36090b9e5b5538df7
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-07-01 22:44:32 +0200

    ests for save

--- modules/web/vue/books/test/data.ts
+++ modules/web/vue/books/test/data.ts
@@ -1,13 +1,15 @@
 import { PouchDB } from '@agrzes/yellow-2020-common-data-pouchdb'
 import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
 import 'mocha'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import {BooksCRUD} from '../src/data'
-const {expect} = chai.use(sinonChai)
+const {expect} = chai.use(sinonChai).use(chaiAsPromised)
 
 class TestClass {
   public field: string
+  public field2?: string
   public static typeTag = 'test'
   public static key(instance: TestClass) {
     return 'key'
@@ -50,6 +52,60 @@ describe('data', function() {
         expect(database.get).to.be.calledOnceWith('test:key')
         expect(result).to.be.instanceOf(TestClass)
         expect(result).to.have.property('field', 'value1')
+      })
+    })
+    describe('save', function() {
+      it('should save data to database', async function() {
+        const database = {
+          put: sinon.mock().returns({field: 'value1'})
+        } as unknown as PouchDB.Database
+        const booksCRUD = new BooksCRUD(database, [])
+        const result = await booksCRUD.save(TestClass, {field: 'value1'})
+        expect(database.put).to.be.calledOnceWith({_id: 'test:key', _rev: undefined, field: 'value1'})
+        expect(result).to.be.instanceOf(TestClass)
+        expect(result).to.have.property('field', 'value1')
+      })
+      it('should merge in case of conflict', async function() {
+        const database = {
+          put: sinon.stub().onFirstCall().throws({name: 'conflict'}).onSecondCall().returns({field: 'value1'}),
+          get: sinon.stub().returns({field: 'value2', field2: 'value2', _rev: 'rev'})
+        } as unknown as PouchDB.Database
+        const booksCRUD = new BooksCRUD(database, [])
+        const result = await booksCRUD.save(TestClass, {field: 'value1'})
+        expect(database.get).to.be.calledOnceWith('test:key')
+        expect(database.put).to.be.calledWith({_id: 'test:key', _rev: undefined, field: 'value1'})
+        expect(database.put).to.be.calledWith({_id: 'test:key', _rev: 'rev', field: 'value1', field2: 'value2'})
+        expect(result).to.be.instanceOf(TestClass)
+        expect(result).to.have.property('field', 'value1')
+        expect(result).to.have.property('field2', 'value2')
+      })
+      it('should fail on other errors', async function() {
+        const error = new Error()
+        const database = {
+          put: sinon.stub().throws(error)
+        } as unknown as PouchDB.Database
+        const booksCRUD = new BooksCRUD(database, [])
+        try {
+          await booksCRUD.save(TestClass, {field: 'value1'})
+          expect.fail('Exception expected')
+        } catch (e) {
+          expect(e).to.be.equals(error)
+        }
+        expect(database.put).to.be.calledWith({_id: 'test:key', _rev: undefined, field: 'value1'})
+      })
+      it('should fail after limited number of retries', async function() {
+        const database = {
+          put: sinon.stub().throws({name: 'conflict'}),
+          get: sinon.stub().returns({field: 'value2', field2: 'value2', _rev: 'rev'})
+        } as unknown as PouchDB.Database
+        const booksCRUD = new BooksCRUD(database, [])
+        try {
+          await booksCRUD.save(TestClass, {field: 'value1'})
+          expect.fail('Exception expected')
+        } catch (e) {
+          expect(e).to.have.property('message', 'Save failed')
+        }
+        expect(database.put).to.be.calledWith({_id: 'test:key', _rev: undefined, field: 'value1'})
       })
     })
   })
