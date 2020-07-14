621ebe17ee6c892b2acb7f21bd8b17d54a6dec22
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-07-14 22:23:34 +0200

    Updated router to new data access

--- modules/web/vue/books/src/routes.ts
+++ modules/web/vue/books/src/routes.ts
@@ -1,31 +1,61 @@
-import { itemComponent, listComponent, registerItemRoute, registerListRoute } from '@agrzes/yellow-2020-web-vue-router'
+import { booksModel } from '@agrzes/yellow-2020-common-books'
+import { registerItemRoute, registerListRoute } from '@agrzes/yellow-2020-web-vue-router'
+import Vue, { VueConstructor } from 'vue'
 import { RouteConfig } from 'vue-router'
 import { AuthorDetails, AuthorList, BookDetails, BooksCards, BooksList, BooksTable } from './components'
+
+export function itemComponent(type: string, theComponent: VueConstructor) {
+  return Vue.extend({
+    template: `<the-component v-if="item" :item="item"></the-component>`,
+    computed: {
+        item() {
+          return booksModel[type][this.$route.params.key]
+        }
+    },
+    components: {
+      theComponent
+    }
+  })
+}
+
+export function listComponent(type: string, theComponent: VueConstructor) {
+  return Vue.extend({
+    template: `<the-component :list="list"></the-component>`,
+    computed: {
+        list() {
+          return booksModel[type]
+        }
+    },
+    components: {
+      theComponent
+    }
+  })
+}
 
 export const bookRoutes: RouteConfig[] = [{
   path: `book/:key`,
   name: `bookDetails`,
-  component: itemComponent('book', BookDetails)
+  component: itemComponent('books', BookDetails)
 }, {
   path: `book`,
   name: `bookList`,
-  component: listComponent('book', BooksList)
+  component: listComponent('books', BooksList)
 }, {
   path: `bookTable`,
   name: `bookTable`,
-  component: listComponent('book', BooksTable)
+  component: listComponent('books', BooksTable)
 }, {
   path: `bookCards`,
   name: `bookCards`,
-  component: listComponent('book', BooksCards)
+  component: listComponent('books', BooksCards)
 }, {
   path: `author/:key`,
   name: `authorDetails`,
-  component: itemComponent('author', AuthorDetails)
+  component: itemComponent('authors', AuthorDetails)
 }, {
   path: `author`,
   name: `authorList`,
-  component: listComponent('author', AuthorList)
+  component: listComponent('authors', AuthorList)
 }]
 
 registerItemRoute('book', 'bookDetails')
