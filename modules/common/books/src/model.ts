eb64c38467f45ba5347db887935556480296da75
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-08-23 18:24:35 +0200

    Model descriptor builder use

--- modules/common/books/src/model.ts
+++ modules/common/books/src/model.ts
@@ -1,104 +1,21 @@
 
-import { Indexer, buildEntities, RelationEntityDescriptor } from '@agrzes/yellow-2020-common-model'
+import { builder, buildEntities, RelationEntityDescriptor } from '@agrzes/yellow-2020-common-model'
 import _ from 'lodash'
 
-export const { book: Book, author: Author, genre: Genre, series: Series, library: Library } = buildEntities([
-  {
-    name: 'Book',
-    typeTag: 'book',
-    key(item: any) {
-      return _.kebabCase(item.title)
-    },
-    label(item: any) {
-      return item.title
-    },
-    relations: [{
-      property: 'author',
-      target: 'author',
-      reverseProperty: 'books'
-    },{
-      property: 'genre',
-      target: 'genre',
-      reverseProperty: 'books'
-    },{
-      property: 'series',
-      target: 'series',
-      reverseProperty: 'books'
-    },{
-      property: 'libraries',
-      nestedProperty: 'library',
-      target: 'library',
-      reverseProperty: 'entries',
-      reverseNestedProperty: 'book'
-    } as RelationEntityDescriptor]
-  },{
-    name: 'Author',
-    typeTag: 'author',
-    key(item: any) {
-      return _.kebabCase(item.name)
-    },
-    label(item: any) {
-      return item.name
-    },
-    relations: [{
-      property: 'books',
-      target: 'book',
-      reverseProperty: 'author'
-    },{
-      property: 'series',
-      target: 'series',
-      reverseProperty: 'author'
-    }]
-  },{
-    name: 'Genre',
-    typeTag: 'genre',
-    key(item: any) {
-      return _.kebabCase(item.name)
-    },
-    label(item: any) {
-      return item.name
-    },
-    relations: [{
-      property: 'books',
-      target: 'book',
-      reverseProperty: 'genre'
-    }]
-  },{
-    name: 'Series',
-    typeTag: 'series',
-    key(item: any) {
-      return _.kebabCase(item.name)
-    },
-    label(item: any) {
-      return item.name
-    },
-    relations: [{
-      property: 'books',
-      target: 'book',
-      reverseProperty: 'series'
-    },{
-      property: 'author',
-      target: 'author',
-      reverseProperty: 'series'
-    }]
-  },{
-    name: 'Library',
-    typeTag: 'library',
-    key(item: any) {
-      return _.kebabCase(item.name)
-    },
-    label(item: any) {
-      return item.name
-    },
-    relations: [{
-      reverseProperty: 'libraries',
-      reverseNestedProperty: 'library',
-      target: 'book',
-      property: 'entries',
-      nestedProperty: 'book'
-    } as RelationEntityDescriptor]
-  }
-])
+const label = (field:string) => _.property<any,string>(field)
+export const { book: Book, author: Author, genre: Genre, series: Series, library: Library } = 
+  builder()
+    .entity('book').label(label('title'))
+      .relation('author').reverseProperty('books')
+      .relation('genre').reverseProperty('books')
+      .relation('series').reverseProperty('books')
+      .relation('libraries').nestedProperty('library').target('library').reverseNestedProperty('book').reverseProperty('entries')
+    .entity('author').label(label('name'))  
+      .relation('series').reverseProperty('author')
+    .entity('genre').label(label('name'))
+    .entity('series').label(label('name'))  
+    .entity('library').label(label('name'))  
+    .build()
 
 export class LibraryEntry<Ref = never> {
   public owned: boolean
