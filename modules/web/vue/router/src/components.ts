7b42587bea5796664b21467343781e4c13278be8
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-09-26 23:08:19 +0200

    Updated router

--- modules/web/vue/router/src/components.ts
+++ modules/web/vue/router/src/components.ts
@@ -1,15 +1,15 @@
-import Vue, { VueConstructor } from 'vue'
+import { Component, defineComponent } from 'vue'
 import 'vue-router'
 import { mapState } from 'vuex'
 import { Entity } from '@agrzes/yellow-2020-common-model'
 
-export function itemComponent(entity: Entity<any>, theComponent: VueConstructor) {
-  return Vue.extend({
+export function itemComponent(entity: Entity<any>, theComponent: Component) {
+  return defineComponent({
     template: `<the-component v-if="item" :item="item"></the-component>`,
     computed: {
       ...mapState('model', {
           item(state: any) {
-              return state.entities[entity.typeTag][this.$route.params.key]
+              return state.entities[entity.typeTag][this.$route.params.key as string]
           }
       })
   },
@@ -19,8 +19,8 @@ export function itemComponent(entity: Entity<any>, theComponent: VueConstructor)
   })
 }
 
-export function listComponent(entity: Entity<any>, theComponent: VueConstructor) {
-  return Vue.extend({
+export function listComponent(entity: Entity<any>, theComponent: Component) {
+  return defineComponent({
     template: `<the-component :list="list"></the-component>`,
     computed: {
       ...mapState('model', {
