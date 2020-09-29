5c7a01b5809faef9cd4adeccd8d9c346c85f0d7d
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-09-29 21:22:27 +0200

    Entity registration

--- modules/web/vue/books/src/index.ts
+++ modules/web/vue/books/src/index.ts
@@ -2,8 +2,12 @@ export * from './components'
 import { registerRoutes } from './routes'
 import { registerEditors } from './components'
 import { Plugin } from '@agrzes/yellow-2020-web-vue-plugin'
+import { Author, Book, Series, Genre, Library, Reading, Plan } from '@agrzes/yellow-2020-common-books'
 
 export const plugin: Plugin ={
   registerRoutes,
-  registerEditors
+  registerEditors,
+  registerModel(registry) {
+    registry.registerModel('book',{Book,Author,Series,Genre,Library,Reading,Plan})
+  }
 }
