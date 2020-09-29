0c640c8de4b2be62ede7d7b6d538a0f771bbab32
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-09-29 07:25:46 +0200

    Plugin mechanism

--- modules/web/vue/plugin/src/index.ts
+++ modules/web/vue/plugin/src/index.ts
@@ -1,1 +1,18 @@
-export * from './routerRegistry'
+import { RouterRegistry, RouterRegistryImpl } from './routerRegistry';
+export {RouterRegistry}
+
+export interface Plugin {
+  registerRoutes?(registry: RouterRegistry): void
+}
+
+export class PluginRegistry {
+  public routerRegistry: RouterRegistry = new RouterRegistryImpl()
+
+  public plugin(plugin: Plugin) {
+    if (plugin.registerRoutes) {
+      plugin.registerRoutes(this.routerRegistry)
+    }
+  }
+}
+
+export const registry = new PluginRegistry()
--- modules/web/vue/plugin/src/routerRegistry.ts
+++ modules/web/vue/plugin/src/routerRegistry.ts
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-
+import { RouteRecordRaw } from 'vue-router'
 
 interface ItemRouteEntry {
   type: string
@@ -8,27 +8,9 @@ interface ItemRouteEntry {
   selectors: string[]
 }
 
-const itemRouteRegistry: ItemRouteEntry[] = []
-
-export function registerItemRoute(type: string, name: string,
-                                  idParam: string = 'key', selectors: string[] = ['default']) {
-  itemRouteRegistry.push( {
-    type,
-    name,
-    idParam,
-    selectors
-  })
-}
-
-export function resolveItemRoute(type: string, id: string, selector: string = 'default') {
-  const entry = _.find(itemRouteRegistry, (e) => e.type === type && _.includes(e.selectors, selector))
-  if (entry) {
-    return {
-      name: entry.name,
-      params: {[entry.idParam]: id}
-    }
-  }
-
+export interface RouteEntry {
+  name: string
+  params?: Record<string,any>
 }
 
 interface ListRouteEntry {
@@ -37,21 +19,57 @@ interface ListRouteEntry {
   selectors: string[]
 }
 
-const listRouteRegistry: ListRouteEntry[] = []
-
-export function registerListRoute(type: string, name: string, selectors: string[] = ['default']) {
-  listRouteRegistry.push( {
-    type,
-    name,
-    selectors
-  })
+export interface RouterRegistry {
+  registerItemRoute(type: string, name: string, idParam?: string , selectors?: string[] ): void
+  resolveItemRoute(type: string, id: string, selector?: string ): RouteEntry
+  registerListRoute(type: string, name: string, selectors?: string[]): void
+  resolveListRoute(type: string, selector?: string): RouteEntry
+  route(route: RouteRecordRaw): void
+  routes: RouteRecordRaw[]
 }
 
-export function resolveListRoute(type: string, selector: string = 'default') {
-  const entry = _.find(listRouteRegistry, (e) => e.type === type && _.includes(e.selectors, selector))
-  if (entry) {
-    return {
-      name: entry.name
+export class RouterRegistryImpl implements RouterRegistry {
+  private itemRouteRegistry: ItemRouteEntry[] = []
+  private listRouteRegistry: ListRouteEntry[] = []
+  public routes: RouteRecordRaw[] = []
+
+  registerItemRoute(type: string, name: string,idParam: string = 'key', selectors: string[] = ['default']) {
+    this.itemRouteRegistry.push( {
+      type,
+      name,
+      idParam,
+      selectors
+    })
+  }
+
+  resolveItemRoute(type: string, id: string, selector: string = 'default') {
+    const entry = _.find(this.itemRouteRegistry, (e) => e.type === type && _.includes(e.selectors, selector))
+    if (entry) {
+      return {
+        name: entry.name,
+        params: {[entry.idParam]: id}
+      }
     }
   }
+  registerListRoute(type: string, name: string, selectors: string[] = ['default']) {
+    this.listRouteRegistry.push( {
+      type,
+      name,
+      selectors
+    })
+  }
+  
+  resolveListRoute(type: string, selector: string = 'default') {
+    const entry = _.find(this.listRouteRegistry, (e) => e.type === type && _.includes(e.selectors, selector))
+    if (entry) {
+      return {
+        name: entry.name
+      }
+    }
+  }
+
+  route(route: RouteRecordRaw): void {
+    this.routes.push(route)
+  }
 }
+
