4db23f01bacc44068d94d81b29a9a871b58206ea
Grzegorz Abramczyk <abramczykg@gmail.com>
2020-09-29 20:20:33 +0200

    Editor registry in plugin

--- modules/web/vue/plugin/src/index.ts
+++ modules/web/vue/plugin/src/index.ts
@@ -1,16 +1,23 @@
+import { EditorRegistry } from './editorRegistry';
 import { RouterRegistry, RouterRegistryImpl } from './routerRegistry';
-export {RouterRegistry}
+export { RouterRegistry }
+export { EditorRegistry }
 
 export interface Plugin {
   registerRoutes?(registry: RouterRegistry): void
+  registerEditors?(registry: EditorRegistry): void
 }
 
 export class PluginRegistry {
   public routerRegistry: RouterRegistry = new RouterRegistryImpl()
+  public editorRegistry: EditorRegistry = new EditorRegistry()
 
   public plugin(plugin: Plugin) {
     if (plugin.registerRoutes) {
       plugin.registerRoutes(this.routerRegistry)
+    }
+    if (plugin.registerEditors) {
+      plugin.registerEditors(this.editorRegistry)
     }
   }
 }
