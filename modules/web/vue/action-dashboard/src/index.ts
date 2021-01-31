96d4c19b0cc8eb30bcd75e22b73731200d698d97
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-01-31 21:39:59 +0100

    Conditional app mounting

--- modules/web/vue/action-dashboard/src/index.ts
+++ modules/web/vue/action-dashboard/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello webpack')
+import {createApp} from 'vue'
+
+const root = document.querySelector('#action-dashboard-host')
+if (root) {
+  const app = createApp({
+    template: `<span>App</span>`,
+  })
+  app.mount(root)
+}
+
