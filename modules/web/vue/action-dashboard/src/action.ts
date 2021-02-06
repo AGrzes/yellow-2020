780a11e5f117094f5a84b9927f26ab5e93439f76
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-02-06 19:45:36 +0100

    Action schema

---
+++ modules/web/vue/action-dashboard/src/action.ts
@@ -0,0 +1,31 @@
+export interface Comment {
+  content: string
+  timestamp?: string
+  author?: string 
+}
+
+export interface Project {
+  name: string
+  priority?: string
+  area?: string[]
+  tags?: string[]
+}
+
+export interface Action {
+  summary: string
+  description?: string
+  comments?: Comment[]
+  project?: Project
+  minimumTime?: string
+  fullTime?: string
+  minimumEnergy?: string
+  context?: string[]
+  location?: string
+  people?: string[]
+  tags?: string[]
+  status: string
+}
+
+export const actions: Action[] = [
+  
+]
