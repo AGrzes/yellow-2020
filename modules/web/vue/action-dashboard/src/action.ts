806d40c681b882d1215140bbd6109bbd763080af
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-02-06 19:52:35 +0100

    Sample data

--- modules/web/vue/action-dashboard/src/action.ts
+++ modules/web/vue/action-dashboard/src/action.ts
@@ -23,9 +23,62 @@ export interface Action {
   location?: string
   people?: string[]
   tags?: string[]
-  status: string
+  status: string,
+  type?: string
 }
 
-export const actions: Action[] = [
-  
-]
+export const actions: Action[] = [{
+  summary: 'Zaproponuj wspólną bibliotekę dla demo cars',
+  status: 'defined',
+  comments:[],
+  context: ['wl'],
+  description: null,
+  fullTime: '1h',
+  minimumTime: '15m',
+  location: null,
+  minimumEnergy: 'high',
+  people: ['piotr dybowski','micah żuchowski'],
+  project: {
+    name: 'Demo Cars',
+    area: ['work'],
+    priority: 'medium',
+    tags: ['tfp','tuatara']
+  },
+  type: 'discuss'
+},{
+  summary: 'Przetestuj prisma dla demo cars',
+  status: 'defined',
+  comments:[],
+  context: ['wl'],
+  description: null,
+  fullTime: '2h',
+  minimumTime: '1h',
+  location: null,
+  minimumEnergy: 'high',
+  people: null,
+  project: {
+    name: 'Demo Cars',
+    area: ['work'],
+    priority: 'medium',
+    tags: ['tfp','tuatara']
+  },
+  type: 'do'
+},{
+  summary: 'Ustal jak z dnia na dzień wizualizować bieżącą pracę',
+  status: 'defined',
+  comments:[],
+  context: ['pc'],
+  description: null,
+  fullTime: '1h',
+  minimumTime: '15m',
+  location: null,
+  minimumEnergy: 'high',
+  people: null,
+  project: {
+    name: 'System improvments',
+    area: ['system'],
+    priority: 'medium',
+    tags: []
+  },
+  type: 'brainstorm'
+}]
