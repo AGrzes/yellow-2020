97e1ed4e09b34ae065e45907685d2654f1233747
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-02-11 13:33:03 +0100

    Fetching actions and projects

--- modules/web/vue/action-dashboard/src/action-source.ts
+++ modules/web/vue/action-dashboard/src/action-source.ts
@@ -1,7 +1,7 @@
 import {JiraClient} from '@agrzes/jira-adapter'
 import axios from 'axios'
 import { toArray, mergeMap,map } from 'rxjs/operators';
-import {Observable, concat, from, of} from 'rxjs'
+import {Observable, concat, from, of, combineLatest} from 'rxjs'
 import PouchDB from 'pouchdb'
 const jiraClient = new JiraClient(axios.create({
   baseURL: process.env.JIRA_URL,
@@ -13,17 +13,29 @@ const jiraClient = new JiraClient(axios.create({
 
 const db = new PouchDB('action-dashboard')
 
-export function actions(): Observable<any[]> {
-  const projects = jiraClient.query(`project='GTD' and type = Project `)
-  projects.subscribe(async (p) => {
-    try {
-      await db.put({_id:p.key, ...p})
-    } catch (e) {
-      if(e.name === 'conflict') {
-        await db.put({_id:p.key,_rev: (await db.get(p.key))._rev, ...p})
-      }
+const save = async (p) => {
+  try {
+    await db.put({_id:p.key, ...p})
+  } catch (e) {
+    if(e.name === 'conflict') {
+      await db.put({_id:p.key,_rev: (await db.get(p.key))._rev, ...p})
     }
-    
-  })
+  }
+  
+}
+
+export function projects(): Observable<any[]> {
+  const projects = jiraClient.query(`project='GTD' and type in (Project, Thread, Activity) `)
+  projects.subscribe(save)
   return concat(from(db.allDocs({include_docs: true})).pipe(mergeMap( r => of(...r.rows)),map(d => d.doc),toArray()),projects.pipe(toArray()))
+}
+
+export function actions(): Observable<any[]> {
+  const actions = jiraClient.query(`project = GTD and (resolutiondate is null or resolutiondate >-1w) and type in (Action,Condition)`)
+  actions.subscribe(save)
+  return concat(from(db.allDocs({include_docs: true})).pipe(mergeMap( r => of(...r.rows)),map(d => d.doc),toArray()),actions.pipe(toArray()))
+}
+
+export function data(): Observable<any[]> {
+  return combineLatest([projects(),actions()]).pipe(map(([p,a]) => ([...p,...a])))
 }
--- modules/web/vue/action-dashboard/src/dashboard.ts
+++ modules/web/vue/action-dashboard/src/dashboard.ts
@@ -1,7 +1,7 @@
 import { defineComponent } from "vue"
 import { actions } from "./action"
 import { actionsToDashboard } from "./actions-to-dashboard"
-import {actions as actionsSource} from './action-source'
+import {data} from './action-source'
 
 export const ActionItem = defineComponent({
   props: {
@@ -82,7 +82,7 @@ export const ActionDashboard = defineComponent({
     ActionList
   },
   mounted() {
-    actionsSource().subscribe((actions) => console.log(actions))
+    data().subscribe((actions) => console.log(actions))
   },
   data() {
     return {
