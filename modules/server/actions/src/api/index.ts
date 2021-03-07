0cf7a1f2a8defb81266fc3a0d8e4b84729af2cd8
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-03-07 08:35:42 +0100

    Web socket server

--- modules/server/actions/src/api/index.ts
+++ modules/server/actions/src/api/index.ts
@@ -1,7 +1,5 @@
 import {json, Router} from 'express'
 import {data} from './action-source'
-import { take } from 'rxjs/operators'
-import  { BehaviorSubject} from 'rxjs'
 const router = Router()
 router.use(json())
 
--- modules/server/actions/src/index.ts
+++ modules/server/actions/src/index.ts
@@ -1,2 +1,32 @@
 import server from './server'
-server.listen(3000)
+import WebSocket from 'ws'
+import {data} from './api/action-source'
+
+const httpServer = server.listen(3000)
+
+const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+httpServer.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, function (ws) {
+    wss.emit('connection', ws, request);
+ })
+});
+
+wss.on('connection',(ws,req) => {
+  console.log('connection')
+  const subscription = data().subscribe({
+    next(i) { 
+      console.log('next')
+      ws.send(JSON.stringify(i))
+    },
+    complete() {
+      console.log('complete')
+      ws.close(1000)
+    },
+    error(e) {
+      console.log('error')
+      ws.close(1011)
+    }
+  })
+  ws.on('close',() => subscription.unsubscribe())
+  ws.on('error',() => subscription.unsubscribe())
+})
